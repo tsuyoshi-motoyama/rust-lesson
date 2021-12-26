c29b578876432abef6bab5adb60e7a8132b765d1
tsuyoshi-motoyama <motoyama@fix-japan.work>
2021-12-26 21:02:40 +0900

    step 12

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-mod stack_heap;
+// mod stack_heap;
+mod ownership;
 // mod vars;
 
 fn main() {
     // println!("Hello, world!");
     // vars::run();
-    stack_heap::run();
+    // stack_heap::run();
     // vars::sub_a::func_a();
     // vars::sub_b::func_b();
+    ownership::run();
 }
